ad65722844257ec1fad29bae460bb38df735d54a
Lee Zhe <o1103015@gm.pu.edu.tw>
2023-03-22 14:10:20 +0800

    v.1(leezhe)

--- app/src/main/java/tw/edu/pu/gm/o1103015/hw0322/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/gm/o1103015/hw0322/MainActivity.kt
@@ -2,10 +2,17 @@ package tw.edu.pu.gm.o1103015.hw0322
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txv:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        txv = findViewById(R.id.txv)
+        txv.setOnClickListener ({
+            txv.text="Hello Noob" })
     }
 }
